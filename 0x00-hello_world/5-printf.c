93a07720c3c9f8c00096b1d6d242f4344edf4a4d
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-03-16 03:38:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints a line of code using puts
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
