2810deae13cd51b6bd0c026a6d16d380a5e5977d
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-03-16 03:08:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("\""Programming is like building a multilingual puzzle");
+        return (0);
+}
