97c09f29b73378ea3d15d429ec8d7ae437bc9ede
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-03-16 03:31:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-/*
- *main - Entry point
+/**
+ * main - A program that prints a line with puts function
  *
- *Return: Always 0 (success)
- *
+ * Return: 0 (success)
  */
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
