a5463fcd29e4d88376c0fa3f58ff573cfee9e365
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-03-21 03:15:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - a program that prints a  line when executed
- *
- * Return: 0 (success)
- */
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
+
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
