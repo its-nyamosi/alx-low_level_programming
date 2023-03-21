65fe8cdccfa7a7cc3efbd98ecd63eabcd7369c1b
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-03-21 04:22:00 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,39 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * main -  times_table - Prints the 9 times table, starting with 0
  *
  * Return: 0 (success)
  */
-void jack_bauer(void)
+void times_table(void)
 {
-    int i = 0;
-    int j = 0;
+	int i, j, k;
 
-    while (i < 24)
-    {
-        j = 0;
-        while (j < 60)
-        {
-            _putchar((i / 10) + '0');
-            _putchar((i % 10) + '0');
-            _putchar(':');
-            _putchar((j / 10) + '0');
-            _putchar((j % 10) + '0');
-            _putchar('\n');
-            j++;
-        }
-        i++;
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
