fcaab0cb2f99572e8c865f20d6b5e4ce14ca8f69
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-03-21 05:53:10 -0700

    100-main.c

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test the print_times_table function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);
+  
+    return (0);
+}
