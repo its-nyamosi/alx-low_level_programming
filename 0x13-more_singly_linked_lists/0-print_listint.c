49d63797f9ee4dcc07fdad57f9e561d17b1ae4f0
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-05-02 00:28:27 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,19 +2,19 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @a: linked list of type listint_t to print
+ * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *a)
+size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
-	while (a)
+	while (h)
 	{
-		printf("%d\n", a->n);
+		printf("%d\n", h->n);
 		num++;
-		a = a->next;
+		h = h->next;
 	}
 
 	return (num);
