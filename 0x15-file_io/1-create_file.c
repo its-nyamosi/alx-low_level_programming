7b498f54ca05f47d1c53e0c1b82a9fabc91472e5
Newton Nyamosi Opera <newtonopera99@gmail.com>
2023-07-10 00:14:35 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - Creates  file.
+ * @fname: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *fname, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (fname == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(fname, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
+
